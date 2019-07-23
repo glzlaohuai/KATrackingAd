5df51948fdf4811f1cd5d141a078fd06a0c9500a
glzlaohuai <494763134@qq.com>
2019-07-23 17:19:50 +0800

    remove unused unity sample project.

--- KATrackingAd_Unity/KATrackingUnitySampleProj/Assets/Plugins/iOS/KATracking/AD/KATrackingUnityConstants.h
+++
@@ -1,9 +0,0 @@
-//
-//  KATrackingUnityConstants.h
-//  DOTest
-//
-//  Created by glzlaohuai on 2018/4/28.
-//  Copyright © 2018年 glzlaohuai. All rights reserved.
-//
-
-#define GAME_OBJ_NAME           "APSDKServiceDelegate"
